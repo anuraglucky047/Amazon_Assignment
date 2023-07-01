29c4d87ddf66c5b15ff284ea369364c2d3529ff2
anuraglucky047 <138308524+anuraglucky047@users.noreply.github.com>
2023-07-02 02:30:56 +0530

    Update Amazon.java

--- Amazon.java
+++ Amazon.java
@@ -1,7 +1,5 @@
-package ajax;
+package assignment;
 
-
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -9,27 +7,49 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Amazon {
 
-	public static void main(String[] args) throws InterruptedException {
 
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+public class Amazon_Search {
+
+	public static void main(String[] args) {
 		
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		
-		
+		//Go to Amazon WebApp:
 		driver.get("https://www.amazon.in/");
 		
-		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("mobiles");
-		Thread.sleep(2000);
-		List<WebElement> suggestions = driver.findElement(By.className("left-pane-results-container")).findElements(By.tagName("div"));
-
-		System.out.println(suggestions.size());
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Wrist Watches");
+		driver.findElement(By.id("nav-search-submit-button")).click();
+		
+		//Search for Display-Analogue:
+		driver.findElement(By.linkText("Analogue")).click();
+		
+		//Search for Brands Material-Leather:
+		driver.findElement(By.linkText("Leather")).click();
+		
+		//Search for Brand-Titan:
+		driver.findElement(By.linkText("Titan")).click();
+		
+		
+		driver.findElement(By.linkText("25% Off or more")).click();
+		
+		//Get the Fifth Element from the search:
+		WebElement element = driver.findElements(By.xpath("//div[@data-component-type='s-search-result']")).get(4);
+		 
+		System.out.println(element.getText());
+		 
+		//Viewing the Fifth Element:
+		element.click();
+		
+		
 	
 		
+
 	}
 
 }
